9707f7388d6195078d627b9869e66dcd8c0a2de1
Triston <tristonherbst97@gmail.com>
2021-05-20 13:29:00 -0500

    user sign up complete functionality complete

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,17 @@
-class API::V1::UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
 
     def create 
-        byebug
+        # byebug
+
+        user = User.create(first_name: params[:firstName], last_name: params[:lastName], email: params[:email],
+         password: params[:password], profile_picture: params[:profilePicture] || nil) 
+        # byebug
+         if user
+            render json: user
+         else
+            render json: {error: 'Something went wrong'}
+         end
+
     end
-    
+
 end
--- db/migrate/20210519214951_create_users.rb
+++ db/migrate/20210519214951_create_users.rb
@@ -6,8 +6,6 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string 'email'
       t.string 'password_digest'
       t.string 'profile_picture'
-      t.integer 'facebookId', :null => true
-      t.integer 'googleId', :null => true
       t.timestamps
     end
   end
